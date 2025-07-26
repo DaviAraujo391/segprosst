36e125c57f73766a0a881bef31ad33da4147ba2c
Davi Araujo <daviaraujo@gmail.com>
2025-07-26 16:27:35 -0300

    Melhorias - Parte 6

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -6,19 +6,35 @@ from . import views
 app_name = 'usuarios'
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='usuarios/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='core:home'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(
+        template_name='usuarios/login.html'), name='login'),
+
+    path('logout/', auth_views.LogoutView.as_view(
+        next_page='core:home'), name='logout'),
+
     path('cadastro/', views.cadastro, name='cadastro'),
     path('painel/', views.painel, name='painel'),
+
+    # Esqueci minha senha
     path('senha-esquecida/', auth_views.PasswordResetView.as_view(
         template_name='usuarios/password_reset.html',
         email_template_name='usuarios/password_reset_email.html',
         subject_template_name='usuarios/password_reset_subject.txt',
         success_url='senha-enviada/'
     ), name='password_reset'),
+
     path('senha-enviada/', auth_views.PasswordResetDoneView.as_view(
         template_name='usuarios/password_reset_done.html'
     ), name='password_reset_done'),
-]
 
+    # CONFIRMAÇÃO e COMPLETAÇÃO da redefinição de senha
+    path('redefinir/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='usuarios/password_reset_confirm.html',
+        success_url='/usuarios/senha-redefinida/'
+    ), name='password_reset_confirm'),
+
+    path('senha-redefinida/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='usuarios/password_reset_complete.html'
+    ), name='password_reset_complete'),
+]
 
