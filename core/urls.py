741b5bd1a11876ead62eb333dedb9b9671adb900
Davi Araujo <daviaraujo@gmail.com>
2025-07-26 14:36:19 -0300

    Melhorias

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from . import views
-from django.urls import path
-from .views import cria_superuser
 
 app_name = 'core'
 
@@ -9,5 +7,4 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('sobre/', views.sobre, name='sobre'),
     path('contato/', views.contato, name='contato'),
-    path('cria-superuser/', cria_superuser, name='cria_superuser'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,19 +1,5 @@
 from django.shortcuts import render
 
-from django.contrib.auth.models import User
-from django.http import HttpResponse
-
-def cria_superuser(request):
-    if not User.objects.filter(username='davia').exists():
-        User.objects.create_superuser(
-            username='davia',
-            email='davi.araujo18@gmail.com',
-            password='admin123'
-        )
-        return HttpResponse("✅ Superusuário criado com sucesso!")
-    else:
-        return HttpResponse("⚠️ Superusuário já existe.")
-
 def home(request):
     return render(request, 'home.html')
 
