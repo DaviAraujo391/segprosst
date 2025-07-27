757cd471ee8d7b5d9cebd883b2d0e95e9d6820fe
Davi Araujo <daviaraujo@gmail.com>
2025-07-27 12:38:44 -0300

    Melhorias - Painel Admin - Parte 4

--- usuarios/models.py
+++ usuarios/models.py
@@ -5,21 +5,24 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-@receiver(post_save, sender=User)
-def criar_perfil_automaticamente(sender, instance, created, **kwargs):
-    if created:
-        Perfil.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def salvar_perfil_automaticamente(sender, instance, **kwargs):
-    instance.perfil.save()
-
 class Perfil(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     cpf = models.CharField(max_length=14)
 
     def __str__(self):
         return f'Perfil de {self.user.username}'
+
+@receiver(post_save, sender=User)
+def criar_ou_salvar_perfil(sender, instance, created, **kwargs):
+    if created:
+        Perfil.objects.create(user=instance)
+    else:
+        # Só tenta salvar se o perfil já existir
+        try:
+            instance.perfil.save()
+        except Perfil.DoesNotExist:
+            # Se não existir, cria o perfil
+            Perfil.objects.create(user=instance)
 
 
 
